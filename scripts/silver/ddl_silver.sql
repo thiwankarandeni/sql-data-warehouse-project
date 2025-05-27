c3a233a5337c75b46ed4836515944cba6b0a4779
thiwankarandeni <74093710+thiwankarandeni@users.noreply.github.com>
2025-05-27 13:51:26 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -31,12 +31,13 @@ GO
 
 CREATE TABLE silver.crm_prd_info (
 	prd_id INT,
+	cat_id NVARCHAR(50),
 	prd_key NVARCHAR(50),
 	prd_nm NVARCHAR(50),
 	prd_cost INT,
 	prd_line NVARCHAR(50),
-	prd_start_dt DATETIME,
-	prd_end_dt DATETIME,
+	prd_start_dt DATE,
+	prd_end_dt DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
@@ -49,9 +50,9 @@ CREATE TABLE silver.crm_sales_details (
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price INT,
@@ -87,10 +88,10 @@ IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
 GO
 
 CREATE TABLE silver.erp_px_cat_g1v2 (
-	cid NVARCHAR(50),
-	cat NVARCHAR(50),
-	subcat NVARCHAR(50),
-	maintenance NVARCHAR(50),
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+    id              NVARCHAR(50),
+    cat             NVARCHAR(50),
+    subcat          NVARCHAR(50),
+    maintenance     NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
